283149d7dc718d0f21597870b3e99af0d2379f33
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:48:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       sender.balance -= amount
       @status = 'complete'
     else
-      puts "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
       @status = 'rejected'
     end
   end
