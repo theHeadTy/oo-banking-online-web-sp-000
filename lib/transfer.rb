b185a34ac8f32d97c48246ba21371306800c8a12
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:46:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if (sender.balance >= amount && valid?)
+    if (sender.balance > amount && valid?)
       receiver.balance += amount
       sender.balance -= amount
       @status = 'complete'
