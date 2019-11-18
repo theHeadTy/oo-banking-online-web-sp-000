4031f54416b310c246bcb888ffdeaa7688a808ab
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:44:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,11 +9,7 @@ class Transfer
   end
 
   def valid?
-    if sender.valid? && receiver.valid?
-      true
-    else
-      false
-    end
+    (sender.valid? && receiver.valid? && status == 'pending') ? true : false
       
   end
 
