a5998a40ec9007ae08e7e08b9f5afecaa22d5f7d
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:53:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,7 @@
 
 class Transfer
   attr_reader :sender, :receiver, :amount, :status
-  def initialize(sender, receiver, amount = 50)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @amount = amount
