9a7b134d2326b1b31ca571ec993826b83aa93c00
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:33:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,7 +9,7 @@ class Transfer
   end
 
   def valid?
-    (sender.valid? && receiver.valid? && amount.is_a? Integer && status == 'open') ? true : false
+    (sender.valid? && receiver.valid? && Integer(amount) && status == 'open') ? true : false
   end
 
   def execute_transaction
