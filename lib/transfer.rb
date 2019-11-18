f310ce2a3dd511bca65cfea83cf119257728aef9
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:44:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @amount = Integer(amount)
+    @amount = amount
     @status = 'pending'
   end
 
