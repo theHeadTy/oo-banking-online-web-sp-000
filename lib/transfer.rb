025bb871a84be4a5a206b480770d2b0eef042d66
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-18 07:31:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
+
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount, :status
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+    (sender.valid? && receiver.valid? && amount.is_a? Integer && status == 'open') ? true : false
+  end
+
+  def execute_transaction
+    if (sender.balance >= amount && valid?)
+      receiver.balance += amount
+      sender.balace -= amount
+      @status = 'complete'
+    else
+      puts "Transaction rejected. Please check your account balance."
+      @status = 'rejected'
+    end
+
+    def reverse_transfer
+      sender.balance += amount
+      receiver.balace -= amount
+    end
 end
